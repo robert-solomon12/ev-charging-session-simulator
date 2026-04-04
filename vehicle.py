4b6d8759011d7944643083ea4c063ed30b7ede2d
root <root@DEDSEC-ALIAS-2.localdomain>
2026-04-04 13:22:01 +0100

    Add ElectricVehicle class for battery charge calculations

---
+++ vehicle.py
@@ -0,0 +1,23 @@
+"""
+vehicle.py
+
+Defines the ElectricVehicle class used in the charging simulation.
+"""
+
+
+class ElectricVehicle:
+    def __init__(self, model_name: str, battery_capacity_kwh: float, current_soc: float):
+        self.model_name = model_name
+        self.battery_capacity_kwh = battery_capacity_kwh
+        self.current_soc = current_soc
+
+    def energy_needed_to_target(self, target_soc: float) -> float:
+        """
+        Calculate how much energy is required to charge
+        from the current SOC to the target SOC.
+        """
+        if target_soc <= self.current_soc:
+            return 0.0
+
+        soc_difference = target_soc - self.current_soc
+        return (soc_difference / 100) * self.battery_capacity_kwh
