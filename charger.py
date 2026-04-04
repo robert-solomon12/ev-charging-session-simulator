ca7a000d8eb85dd706512e4aec03a3aeb7847a0c
root <root@DEDSEC-ALIAS-2.localdomain>
2026-04-04 13:50:47 +0100

    Add Charger class with power and pricing attributes

---
+++ charger.py
@@ -0,0 +1,12 @@
+"""
+charger.py
+
+Defines the Charger class.
+"""
+
+
+class Charger:
+    def __init__(self, charger_name: str, power_kw: float, price_per_kwh: float):
+        self.charger_name = charger_name
+        self.power_kw = power_kw
+        self.price_per_kwh = price_per_kwh
